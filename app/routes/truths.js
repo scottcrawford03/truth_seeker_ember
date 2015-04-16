ae4dc117f9febd0cf77270df4a81576f52d20f0d
Konr Larson <larsonkonr@gmail.com>
2015-04-16 11:38:03 -0600

    changed route to include ember

--- app/routes/truths.js
+++ app/routes/truths.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return $.getJSON('/api/v1/categories').then( (data) => {
+    return Ember.$.getJSON('/api/v1/categories').then( (data) => {
       return data.categories.map( (category) => category );
     });
   }
