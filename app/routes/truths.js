fd0055e8f75232314c1c080fed44f40d9f8e641b
Scott Crawford <scottcrawford03@gmail.com>
2015-04-16 16:42:00 -0600

    working on deploy

--- app/routes/truths.js
+++ app/routes/truths.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
+import ENV from 'truth-seeker-ember/config/environment'
+
+const url = ENV.apiUrl || '';
 
 export default Ember.Route.extend({
   model: function () {
-    return $.getJSON('/api/v1/categories').then( (data) => {
+    return $.getJSON(`${url}/api/v1/categories`).then( (data) => {
       return data.categories.map( (category) => category );
     });
   }
--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.apiUrl = '//developer-truth-seeker.herokuapp.com'
   }
 
   ENV.mapStyles = [
